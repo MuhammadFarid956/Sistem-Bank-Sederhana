dd963bd401aa91cf1d152c06b3841cd1f323d168
MuhammadFarid956 <mhmmdfaridirfn@gmail.com>
2026-02-12 23:35:04 +0700

    .

--- accounts.py
+++ accounts.py
@@ -1,56 +1,61 @@
-from operational import Utility
+import random
 
-# FILENAME = "accounts.txt"
-
-class Accounts:
-    def __init__(self, account_name, initial_deposit):
-        self.acc_num = Utility.gen_acc_num()
+class Account:
+    def __init__(self, account_name, initial_deposit, account_num= None):
         self.acc_name = account_name
-        self.acc_balance = initial_deposit
-        self.accounts = []
+        self.acc_balance = float(initial_deposit)
+        self.acc_num = account_num if account_num else self.gen_acc_num()
 
-    def to_list(self):
-        return [self.acc_num, self.acc_name, self.acc_balance]
-
-    def create_acc(self, new_acc_num):
-        self.acc_num = new_acc_num
-        new_acc = self.to_list()
-        self.accounts.append(new_acc)
-        Utility.save_data()
-        return new_acc
+    @staticmethod
+    def gen_acc_num():
+        return ''.join(str(random.randint(0, 9) for _ in range(10)))
 
     def deposit(self, amount):
         if amount > 0:
             self.acc_balance += amount
-            print(f"Deposited ${amount}. New balance : ${self.acc_balance}")
-        else:
-            print("Invalid deposit amount.")
+            print(f"Deposited ${amount}. New balance : ${self.acc_balance:.2f}")
+            return True
+        print("Invalid deposit amount.")
+        return False
 
     def withdraw(self, amount):
         if amount > 0 and amount < self.balance:
-            self.balance -= amount
-            print(f"Withdrew ${amount}. New balance : ${self.balance}")
-        else:
-            print("Invalid withdrawal amount or insufficient founds")
+            self.acc_balance -= amount
+            print(f"Withdrew ${amount}. New balance : ${self.acc_balance:.2f}")
+            return True
+        print("Invalid withdrawal amount or insufficient founds")
+        return False
 
-    def check_balance(self):
-        print(f"Current balance for account {self.account_number}: ${self.balance} ")
+    def to_list(self): #Konversi data to CSV file
+        return [self.acc_num, self.acc_name, self.acc_balance]
 
-    def display_details(self):
-        print("\n--- Account Details ---")
-        print(f"Account Holder : {self.name}\nAccount Number : {self.account_number}\nBalance : {self.balance}")
-        print("-" * 30)
+    def __str__(self):
+        return f"Name : {self.acc_name} | Number Account : {self.acc_num} | Balance : ${self.acc_balance:.2f}"
 
-    # def __init__(self, name, initial_deposit):
-    #     self.name = name
-    #     self.balance = initial_deposit
-    #     self.account_number = self.generate_account_number()
-    #     print(f"Account for {self.name} created successfully.")
-    #     print(f"Your Number is: {self.account_number}")
+    # def create_acc(self, new_acc_num):
+    #     self.acc_num = new_acc_num
+    #     new_acc = self.to_list()
+    #     self.accounts.append(new_acc)
+    #     Utility.save_data()
+    #     return new_acc
     #
-    # def generate_account_number(self):
-    #     # Simple generate a 10-digit account number
-    #     return ''.join([str(random.randint(0,9)) for _ in range(10)])
-if __name__ == "__main__":
-    user = Accounts
-    user.create_acc('')
+    #
+    #
+    # def check_balance(self):
+    #     print(f"Current balance for account {self.account_number}: ${self.balance} ")
+    #
+    # def display_details(self):
+    #     print("\n--- Account Details ---")
+    #     print(f"Account Holder : {self.name}\nAccount Number : {self.account_number}\nBalance : {self.balance}")
+    #     print("-" * 30)
+    #
+    # # def __init__(self, name, initial_deposit):
+    # #     self.name = name
+    # #     self.balance = initial_deposit
+    # #     self.account_number = self.generate_account_number()
+    # #     print(f"Account for {self.name} created successfully.")
+    # #     print(f"Your Number is: {self.account_number}")
+    # #
+    # # def generate_account_number(self):
+    # #     # Simple generate a 10-digit account number
+    # #     return ''.join([str(random.randint(0,9)) for _ in range(10)])
--- operational.py
+++ operational.py
@@ -1,35 +1,33 @@
 import csv
 import os
-import random
-from accounts import Accounts
+from accounts import Account
 
 FILENAME = "accounts_data.csv"
 
-class Utility:
-    def __init__(self):
-        self.accounts = []
-        self.load_data()
-
-    def load_data(self):
+class BankStorage:
+    @staticmethod
+    def load_data():
+        accounts = {}
         if not os.path.exists(FILENAME):
-            return
+            return accounts
 
         try:
             with open(FILENAME, 'r') as file:
                 reader = csv.reader(file)
-                self.accounts = [Accounts(*row) for row in reader if row]
-        except FileNotFoundError:
-            print("File not found.")
+                for row in reader:
+                    if row in  reader:
+                        new_acc = Account(account_name=row[1], initial_deposit=row[2], account_num=row[0])
+                        accounts[new_acc.acc_num] = new_acc
+            return accounts
+        except Exception as e:
+            print(f"File not found: {e}")
+            return{}
 
-    def save_data(self):
+    @staticmethod
+    def save_data(accounts_dict):
         try:
             with open(FILENAME, 'w', newline='') as file:
                 writer = csv.writer(file)
-                writer.writerows(acc.to_list() for acc in self.accounts)
-        except FileNotFoundError:
-            print("File not found. Can't save data.")
-
-    @staticmethod
-    def gen_acc_num():
-        return ''.join(str(random.randint(0,9) for _ in range(10)))
-
+                writer.writerows(acc.to_list() for acc in accounts_dict.values())
+        except Exception as e:
+            print(f"File not found. Can't save data: {e}")
